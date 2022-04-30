38d9d7dba6badf79d7e79f45c459f5768f74b589
LeoSerinhano <leonardo.serinhano@gmail.com>
2022-04-29 22:44:35 -0300

    Create Idade_em_Dias.js

---
+++ Desafios/javaScript/Idade_em_Dias.js
@@ -0,0 +1,19 @@
+/*
+Você terá o desafio de ler um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
+Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+*/
+
+let totalDeDias = 400;//parseInt(gets())
+let qtdAnos, qtdMeses;
+
+qtdAnos = parseInt(totalDeDias / 365);
+qtdMeses= parseInt(((totalDeDias % 365) / 30));
+totalDeDias= ((totalDeDias % 365) % 30);;
+
+let resultado = (qtdAnos + " ano(s) " + '\n' + 
+qtdMeses +" mes(es)" + '\n' +
+totalDeDias + " dia(s)");
+
+//print(resultado);
+console.log(resultado);
