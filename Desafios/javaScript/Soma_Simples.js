c023fcd837fbcf87ab1063ec4f6668e4cc362408
LeoSerinhano <leonardo.serinhano@gmail.com>
2022-04-29 22:17:17 -0300

    Create Soma_Simples.js

---
+++ Desafios/javaScript/Soma_Simples.js
@@ -0,0 +1,12 @@
+/*
+Leia dois valores inteiros identificados como variáveis A e B. Calcule a soma entre elas e chame essa variável de SOMA.
+A seguir escreva o valor desta variável.
+*/
+
+let A = 30; // parseInt(gets())
+let B = 10; // parseInt(gets())
+
+let SOMA = A+B;               
+
+//print("SOMA = "+ SOMA );
+console.log("SOMA = "+ SOMA);
