440318ca47d0069a1fa1dce1633438d671304fef
LeoSerinhano <leonardo.serinhano@gmail.com>
2022-05-02 15:05:16 -0300

    Create Exibindo_Numeros_Pares.js

---
+++ Desafios/javaScript/Exibindo_Numeros_Pares.js
@@ -0,0 +1,17 @@
+/*
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+*/
+
+let i = 6;// parseInt(gets())
+let par = 1;
+
+ 
+while(par<=i){
+
+ if (par%2==0){
+      console.log(par)
+ } 
+
+par++
+
+}
